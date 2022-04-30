7ef099de46cb4f29c230fbc9a3996b376bb99233
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2022-04-29 21:09:39 -0300

    Tipos Basicos

---
+++ capitulo1/PrimeiroProjeto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte n1 = -127;
+            short n2 = -32768;
+
+            byte n3 = 255;
+
+            n3++;
+
+            Console.WriteLine($"{n1}: {n1.GetType()}");
+            Console.WriteLine($"{n2}");
+
+            Console.WriteLine($"Overflow: {n3}");
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
