07021c8e7d63d6dea70131b496ef8292c4dbdb62
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2022-04-30 15:47:29 -0300

    Primeiro Exercicio Resolvido

---
+++ capitulo1/PrimeiroProjeto/output/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PrimeiroProjeto.Output
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.
+        }
+    }
+}
---
+++ exercicios1/Exercicio1A/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+
+            int soma = n1 + n2;
+
+            Console.WriteLine($"A soma dos dois números informados é: {soma}");
+        }
+    }
+}
