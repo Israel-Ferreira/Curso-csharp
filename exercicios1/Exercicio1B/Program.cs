58f577c3a1a456a475e31187db0024e9fed9b03e
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2022-04-30 15:57:15 -0300

    Segundo Exercicio da Lista I, resolvido

---
+++ exercicios1/Exercicio1B/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Exercicio1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           const double PI = 3.14159;
+
+           double raio;
+
+           Console.WriteLine("Informe o raio do circulo: ");
+           raio = Convert.ToDouble(Console.ReadLine());
+
+           double areaCirculo =  PI * (Math.Pow(raio, 2));
+
+           Console.WriteLine($"A área do circulo é: {areaCirculo} m**2");
+        }
+    }
+}
