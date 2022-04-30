565fdcc8abe5dcdf71a999535fe72f9b9a49ec0b
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2022-04-30 18:44:15 -0300

    Placeholders

---
+++ capitulo1/OutputCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace OutputCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numeroDouble = 34.99982;
+
+            Console.WriteLine(numeroDouble); // Imprimindo o Número sem a formatação da quantidade de casas decimais
+            Console.WriteLine(numeroDouble.ToString("F2")); // Imprimindo o Número com duas casas decimais
+
+            Console.WriteLine(numeroDouble.ToString("F2", CultureInfo.InvariantCulture));
+            
+            string nome = "Israel";
+            int idade = 22;
+
+
+            Console.WriteLine("O funcionario {0} tem {1} anos", nome, idade);
+
+        }
+    }
+}
