3c01d7301faf66b7187519941bd6b3cc515137cf
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2022-04-30 16:51:15 -0300

    Terceiro Exercicio Resolvido

---
+++ exercicios1/Exercicio1C/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Exercicio1C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+
+            Console.WriteLine("Informe o primeiro numero: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo numero: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o terceiro numero: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o quarto numero: ");
+            d = Convert.ToInt32(Console.ReadLine());
+
+            double diff = (a * b) - (c * d);
+
+            Console.WriteLine($"DIFERENCA={diff}");
+ 
+        }
+    }
+}
