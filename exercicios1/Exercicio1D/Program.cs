6c6936762dfbc495395356c47fc92621b46dc6fd
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2022-04-30 18:23:43 -0300

    Exercicio 4

---
+++ exercicios1/Exercicio1D/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Exercicio1D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double valorPorHora;
+
+            int horasTrabalhadas;
+
+            Console.WriteLine("Informe o número/inscrição do funcionario: ");
+            string inscrição = Console.ReadLine();
+
+            Console.WriteLine("Informe as horas trabalhadas pelo funcionario: ");
+            horasTrabalhadas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor pago por hora para o funcionario: ");
+            valorPorHora = Convert.ToDouble(Console.ReadLine());
+
+
+            double salario = horasTrabalhadas * valorPorHora;
+
+            Console.WriteLine($"O funcionario com a inscrição {inscrição} recebera: R$ {salario}");
+
+        }
+    }
+}
