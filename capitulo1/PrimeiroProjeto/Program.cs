175515997b59c23d32d52321b86a5e75d82b45a9
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2022-04-29 21:25:47 -0300

    Int & Long

--- capitulo1/PrimeiroProjeto/Program.cs
+++ capitulo1/PrimeiroProjeto/Program.cs
@@ -9,7 +9,10 @@ namespace PrimeiroProjeto
             sbyte n1 = -127;
             short n2 = -32768;
 
-            byte n3 = 255;
+            byte n3 = 255; // Tipo Byte (Somente números Positivos de 0 a 255)
+            int valorInt = 10000; // Tipo Inteiro
+
+            long n4 = 2147483648455L;
 
             n3++;
 
@@ -18,7 +21,9 @@ namespace PrimeiroProjeto
 
             Console.WriteLine($"Overflow: {n3}");
 
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"{valorInt * 2}, Tipo {valorInt.GetType()}");
+
+            Console.WriteLine($"Valor long: {n4}, Tipo: {n4.GetType()}");
         }
     }
 }
